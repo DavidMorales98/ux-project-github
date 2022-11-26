67f59e90b1737b7ba2fba8002f044b862cc7e7dc
DavidMorales98 <david.morales.p@usach.cl>
2022-11-26 19:49:55 -0300

    fix backend / bd

--- ux-project/UX-BACK/src/controllers/index.controllers.js
+++ ux-project/UX-BACK/src/controllers/index.controllers.js
@@ -7,74 +7,85 @@ const pool = new Pool({
     port: 5432
 });
 
-const conexion = (req, res) =>{
+const conexion = (req, res) => {
     res.send('Conexión a la base de datos exitosa!');
 };
 
-const getUsuario = async (req, res) =>{
+const getUsuario = async (req, res) => {
     const response = await pool.query('SELECT * from t_usuario\
         WHERE rut = $1', [req.params.rut]);
     res.send().json(response.rows);
 };
 
-const getCuentas = async (req, res) =>{
+const getCuentas = async (req, res) => {
     const response = await pool.query('SELECT C.n_cuenta, C.tipo_cuenta, C.saldo, B.nombre FROM t_cuenta C, t_banco B\
         WHERE C.rut_usuario = $1\
-        and C.id_banco = B.id',[req.params.rut]);
+        and C.id_banco = B.id', [req.params.cta]);
     res.send().json(response.rows);
 };
 
-const getAbonos = async (req, res) =>{
+const getAbonos = async (req, res) => {
     const response = await pool.query('SELECT DISTINCT tr.n_operacion, bn.nombre, tr.n_cuenta_origen, ct.tipo_cuenta, us.nombre, "ABONO" as tipo, tr.monto FROM t_cuenta as ct, t_transferencia as tr, t_usuario as us, t_banco as bn\
         WHERE tr.n_cuenta_destino = $1\
         and ct.n_cuenta = tr.n_cuenta_origen \
         and us.rut = ct.rut_usuario\
-        and bn.id = ct.id_banco',[req.params.rut]);
+        and bn.id = ct.id_banco', [req.params.cta]);
     res.send().json(response.rows);
 };
 
-const getCargos = async (req, res) =>{
+const getCargos = async (req, res) => {
     const response = await pool.query('SELECT DISTINCT tr.n_operacion, bn.nombre, tr.n_cuenta_destino, ct.tipo_cuenta, us.nombre, "CARGO" as tipo, tr.monto\
         FROM t_cuenta as ct, t_transferencia as tr, t_usuario as us, t_banco as bn\
         WHERE tr.n_cuenta_origen = $1\
         and ct.n_cuenta = tr.n_cuenta_destino\
         and us.rut = ct.rut_usuario \
-        and bn.id = ct.id_banco',[req.params.rut]);
+        and bn.id = ct.id_banco', [req.params.rut]);
     res.send().json(response.rows);
 };
 
-const getContactos = async (req, res) =>{
+const getContactos = async (req, res) => {
     const response = await pool.query('select U.rut, U.nombre, C.n_cuenta, CC.tipo_cuenta, CC.saldo, B.nombre\
         from t_contacto C, t_usuario U, t_cuenta CC, t_banco B\
         where C.rut_usuario1 = $1\
         and C.rut_usuario2 = U.rut\
         and C.n_cuenta = CC.n_cuenta\
-        and CC.id_banco = B.id',[req.params.rut]);
+        and CC.id_banco = B.id', [req.params.rut]);
     res.send().json(response.rows);
 };
 
-const postContacto = async (req, res) =>{
+const postContacto = async (req, res) => {
     const rut = req.params.rut;
-    const {rut_usuario2, n_cuenta}  = req.body.params;
+    const { rut_usuario2, n_cuenta } = req.body.params;
     const response = await pool.query('INSERT INTO t_contactos\
     (rut_usuario1, rut_usuario2, n_cuenta)\
     VALUES ($1, $2, $3)',
-    rut,rut_usuario2,n_cuenta);
+        rut, rut_usuario2, n_cuenta);
     res.send('Contacto añadido!');
 };
 
-const deleteContacto = async (req, res ) =>{
-    const rut= req.params.rut;
-    const {rut_usuario2, n_cuenta} = req.body.params;
+const deleteContacto = async (req, res) => {
+    const rut = req.params.rut;
+    const { rut_usuario2, n_cuenta } = req.body.params;
     const response = await pool.query('delete\
     from t_contacto\
     where rut_usuario1 = $1\
     and rut_usuario2 = $2\
     and n_cuenta = $3',
-    rut,rut_usuario2,n_cuenta);
+        rut, rut_usuario2, n_cuenta);
     res.send('Contacto eliminado correctamente!')
 };
 
+const postTransferencia = async (req, res) => {
+    const cuenta = req.params.cta;
+    const { monto, comentario, n_cuenta_destino } = req.body.params;
+    var hoy = new Date();
+    var fecha = hoy.getDate() + '-' + (hoy.getMonth() + 1) + '-' + hoy.getFullYear();
+    var hora = hoy.getHours() + ':' + hoy.getMinutes() + ':' + hoy.getSeconds();
+    const response = await pool.query('insert into t_transferencia\
+    values ($1,$2,$3,$4,$5,$6)',
+        monto, comentario, fecha, hora, cuenta, n_cuenta_destino);
+    res.send('Transferencia realizada con exito!')
+};
 module.exports = {
     conexion,
     getUsuario,
@@ -83,5 +94,8 @@ module.exports = {
     getCargos,
     getContactos,
     postContacto,
-    deleteContacto
+    deleteContacto,
+    postTransferencia
 }
+
+
--- ux-project/UX-BACK/src/routes/index.js
+++ ux-project/UX-BACK/src/routes/index.js
@@ -9,7 +9,8 @@ getAbonos,
 getCargos,
 getContactos,
 postContacto,
-deleteContacto
+deleteContacto,
+postTransferencia
 } = require('../controllers/index.controllers.js');
 
 
@@ -25,16 +26,12 @@ router.get('/cuentas/:rut', getCuentas);//Cuentas asociadas a partir un RUT
 router.get('/abono/:cta', getAbonos); //Abonos asociados a partir de un n_cuenta
 router.get('/cargo/:cta', getCargos); //Cargos asociados a partir de un n_cuenta
 
-
 //Contacts
 router.get('/contactos/:rut', getContactos); //Información contactos asociados a partir de un RUT
 router.post('/contacto/:rut', postContacto); //Asociar un contacto a un RUT
 router.delete('/contacto/:rut', deleteContacto); //Eliminar un contacto asociado a un RUT
 
 //Transfer
-router.get('/transferencia/:id');
-router.post('/transferir/:n_cuenta')
-
-
+router.post('/transferir/:n_cuenta', postTransferencia); //Realizar transferencia a partir de una cuenta
 
 module.exports = router;
